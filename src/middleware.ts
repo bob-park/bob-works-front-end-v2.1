8f61dd1534b5be010b449ab22ee778196e9a17c1
Bob Park <hwpark11@kakao.com>
2023-08-06 01:20:07 +0900

    feat: middleware 로 api call 시 401 인 경우 login page 로 redirect 될 수 있도록 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,5 +3,18 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function middleware(req: NextRequest) {
   const response = NextResponse.next();
 
+  if (req.nextUrl.pathname.startsWith('/api')) {
+    // api call 중 unauthorized 발생시 로그인 페이지로 redirect
+    if (response.status === 401) {
+      return NextResponse.redirect(
+        new URL('/api/oauth2/authorization/bob-works', req.url),
+      );
+    }
+  }
+
   return response;
 }
+
+// export const config = {
+//   matcher: ['/api:path*'],
+// };
