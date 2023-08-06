be42fab17919a91d69de3101d0d62dfe03f7b020
Bob Park <hwpark11@kakao.com>
2023-08-06 20:06:12 +0900

    fix: logout 시 정상적으로 로그아웃 되지 않는 문제 수정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      // {
+      //   source: '/api/logout',
+      //   destination: process.env.CLIENT_SERVICE_PATH + '/logout',
+      // },
+      {
+        source: '/api/oauth2/authorization/bob-works',
+        destination:
+          process.env.CLIENT_SERVICE_PATH + '/oauth2/authorization/bob-works',
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,22 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+// * url
 const API_PREFIX = '/api';
+const AUTH_URL = '/api/oauth2/authorization/bob-works';
+const LOGOUT_URL = '/api/logout';
+
 const CLIENT_SERVICE_PATH = process.env.CLIENT_SERVICE_PATH;
 const COOKIE_SESSION_ID = 'JSESSIONID';
+
+const AUTH_UNCHECK_URLS = [AUTH_URL];
+
+function checkedAuth(url: string) {
+  if (AUTH_UNCHECK_URLS.some((checkedUrl) => checkedUrl == url)) {
+    return false;
+  }
+
+  return url.startsWith(API_PREFIX);
+}
 
 async function apiCall(
   url: string,
@@ -24,10 +38,11 @@ async function apiCall(
 }
 
 export async function middleware(req: NextRequest) {
+  const pathname = req.nextUrl.pathname;
   const response = NextResponse.next();
 
-  if (req.nextUrl.pathname.startsWith(API_PREFIX)) {
-    const requestUrl = req.nextUrl.pathname.substring(API_PREFIX.length);
+  if (checkedAuth(pathname)) {
+    const requestUrl = pathname.substring(API_PREFIX.length);
 
     const params = req.nextUrl.searchParams;
 
@@ -51,16 +66,16 @@ export async function middleware(req: NextRequest) {
       );
     }
 
+    if (pathname.startsWith(LOGOUT_URL)) {
+      return NextResponse.redirect(
+        new URL('/api/oauth2/authorization/bob-works', req.url),
+      );
+    }
+
     return new NextResponse(apiResponse.body, {
       status: apiResponse.status,
       statusText: apiResponse.statusText,
     });
-
-    // if (response.status === 401) {
-    //   return NextResponse.redirect(
-    //     new URL('/api/oauth2/authorization/bob-works', req.url),
-    //   );
-    // }
   }
 
   return response;
