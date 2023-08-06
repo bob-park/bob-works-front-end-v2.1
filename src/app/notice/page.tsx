514fcb872dbbe8fc51809fccc089038b36dfaa72
Bob Park <hwpark11@kakao.com>
2023-08-06 21:21:26 +0900

    feat: notice page 추가

---
+++ src/app/notice/page.tsx
@@ -0,0 +1,15 @@
+import NoticeList from './NoticeList';
+
+export default function NoticePage() {
+  return (
+    <main className="w-full h-full">
+      <div className="grid grid-cols-1 gap-8">
+        <div className="my-4">
+          <h2 className="text-2xl font-bold">공지</h2>
+        </div>
+        {/* notice list */}
+        <NoticeList />
+      </div>
+    </main>
+  );
+}
