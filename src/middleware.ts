515f184cf8573c04424f260faee327cfe3ddb40a
Bob Park <hwpark11@kakao.com>
2023-08-06 21:42:46 +0900

    fix: proxy 의 request 가 정상적으로 요청되지 않는 문제 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -21,17 +21,14 @@ function checkedAuth(url: string) {
 async function apiCall(
   url: string,
   method: string,
+  headers: HeadersInit,
   params?: URLSearchParams,
   body?: any,
-  sessionId?: string,
 ) {
   const response = await fetch(`${CLIENT_SERVICE_PATH + url}?` + params, {
     method,
-    body: JSON.stringify(body),
-    headers: {
-      Cookie: `${COOKIE_SESSION_ID}=${sessionId}`,
-      'Content-Type': 'application/json',
-    },
+    body,
+    headers,
   });
 
   return response;
@@ -46,18 +43,14 @@ export async function middleware(req: NextRequest) {
 
     const params = req.nextUrl.searchParams;
 
-    let body;
-
-    if (req.method === 'POST' || req.method === 'PUT') {
-      body = await req.json();
-    }
+    let body = req.body;
 
     const apiResponse = await apiCall(
       requestUrl,
       req.method,
+      req.headers,
       params,
       body,
-      req.cookies.get(COOKIE_SESSION_ID)?.value,
     );
 
     if (apiResponse.status === 401) {
