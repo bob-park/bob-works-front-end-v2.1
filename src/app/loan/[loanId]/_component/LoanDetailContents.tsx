6bbf4159cdfe50f37413b756bd9d79b4a0f4b3b6
Bob Park <hwpark11@kakao.com>
2024-05-07 19:02:53 +0900

    feat: type 추가

--- src/app/loan/[loanId]/_component/LoanDetailContents.tsx
+++ src/app/loan/[loanId]/_component/LoanDetailContents.tsx
@@ -7,6 +7,19 @@ type LoanDetailContentsProps = {
   loanId: number;
 };
 
+function parseRepaymentType(type: LoanRepaymentType) {
+  switch (type) {
+    case 'LEVEL_PAYMENT':
+      return '원리금 균등 분할 상환';
+    case 'EQUAL_PRINCIPAL_PAYMENT':
+      return '원금 균등 분할 상환';
+    case 'BALLOON_PAYMENT':
+      return '만기 일시 상환';
+    default:
+      return '사용자 설정';
+  }
+}
+
 export default function LoanDetailContents({
   loanId,
 }: LoanDetailContentsProps) {
@@ -39,19 +52,29 @@ export default function LoanDetailContents({
       <div className="col-span-1 text-right">
         <span>추가 설명</span> :
       </div>
-      <div className="col-span-4">
+      <div className="col-span-4 ">
         <p>{loan.description}</p>
       </div>
+
+      {/* 상환 종류 */}
+      <div className="col-span-1 text-right">
+        <span>상환 방식</span> :
+      </div>
+      <div className="col-span-2 text-xl font-bold text-right">
+        <p>{parseRepaymentType(loan.repaymentType)}</p>
+      </div>
+      <div className="col-span-2"></div>
 
       {/* 대출 기간 */}
       <div className="col-span-1 text-right">
         <span>대출 기간</span> :
       </div>
-      <div className="col-span-4 text-xl font-bold">
+      <div className="col-span-2 text-xl text-right font-bold">
         <span>{dayjs(loan.startDate).format('YYYY-MM-DD')}</span>
         <span> ~ </span>
         <span>{dayjs(loan.endDate).format('YYYY-MM-DD')}</span>
       </div>
+      <div className="col-span-2"></div>
 
       {/* 이자 (%) */}
       <div className="col-span-1 text-right">
